b531df1de5febcaa43c096309ea2bff7c1be8622
vineetha <marganavineetha1711@gmail.com>
2025-03-28 16:47:42 +0530

    Storage-vineetha

--- supabase/functions/_handler/_meme_module/uploadmemetoprivatebucket.ts
+++ supabase/functions/_handler/_meme_module/uploadmemetoprivatebucket.ts
@@ -11,21 +11,24 @@ export default async function uploadPrivateMeme(req: Request) {
       return new Response(JSON.stringify({ error: "No file uploaded" }), { status: 400 });
     }
 
-    const filePath = `uploads/${Date.now()}_${file.name}`;
+    const filePath = `memes/${Date.now()}_${file.name}`;
+    console.log(filePath);
 
     const { error: uploadError } = await supabase.storage
       .from("meme")
       .upload(filePath, file, { upsert: false });
 
     if (uploadError) {
+        console.error(uploadError);
       return new Response(JSON.stringify({ error: uploadError.message }), { status: 500 });
     }
-
+    console.log("File uploaded successfully");
     const { data: signedUrlData, error: urlError } = await supabase.storage
       .from("meme")
-      .createSignedUrl(filePath, 60 * 60);
+      .createSignedUrl(filePath, 80);
 
     if (urlError) {
+        console.error(urlError);
       return new Response(JSON.stringify({ error: urlError.message }), { status: 500 });
     }
 
